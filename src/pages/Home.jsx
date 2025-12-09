2e33832d4b4cfd1ef84585eadcbea30c727c9ed5
Ewan-Dkhar <wanedarkh@gmail.com>
2025-12-10 00:57:47 +0530

    Fix Home page issue.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "../styles/Home.css";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
   const images = [
@@ -7,6 +8,8 @@ const Home = () => {
     "https://images.pexels.com/photos/694740/pexels-photo-694740.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     "https://images.pexels.com/photos/4581325/pexels-photo-4581325.jpeg"
   ];
+
+  const navigate = useNavigate()
 
   const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -50,69 +53,77 @@ const Home = () => {
 
           <div className="book-grid">
             {/* BOOK 1 */}
-            <div className="book-card">
+            <div className="book-card" onClick={() => {navigate(`/product/1`)}}>
               <img
                 src="https://emedicodiary.com/images/books/64cc1fc52646aa176937d4ae350e11fa.jpg"
                 className="b1"
                 alt="The Silent Patient"
               />
-              <h3>The Silent Patient</h3>
-              <p className="genre">Horror</p>
-              <p className="desc">
-                The night comfort in the stillness of the late hours...
-              </p>
-              <p className="price">Rs. 499</p>
+              <div>
+                <h3>The Silent Patient</h3>
+                <p className="genre">Horror</p>
+                <p className="desc">
+                  The night comfort in the stillness of the late hours...
+                </p>
+                <p className="price">Rs. 499</p>
 
-              <button>Buy Now</button>
-              <button className="secondary">Add to Cart</button>
+                <button onClick={() => {navigate(`/checkout/1`)}}>Buy Now</button>
+                <button className="secondary">Add to Cart</button>
+              </div>
             </div>
 
             {/* BOOK 2 */}
-            <div className="book-card">
+            <div className="book-card" onClick={() => {navigate(`/product/1`)}}>
               <img src="https://m.media-amazon.com/images/I/81wgcld4wxL.jpg" />
-              <h3>Atomic Habits</h3>
-              <p className="genre">Self Help</p>
-              <p className="desc">
-                Consistent changes lead to big improvements...
-              </p>
-              <p className="price">Rs. 599</p>
+              <div>
+                <h3>Atomic Habits</h3>
+                <p className="genre">Self Help</p>
+                <p className="desc">
+                  Consistent changes lead to big improvements...
+                </p>
+                <p className="price">Rs. 599</p>
 
-              <button>Buy Now</button>
-              <button className="secondary">Add to Cart</button>
+                <button onClick={() => {navigate(`/checkout/1`)}}>Buy Now</button>
+                <button className="secondary">Add to Cart</button>
+              </div>
             </div>
 
             {/* BOOK 3 */}
-            <div className="book-card">
+            <div className="book-card" onClick={() => {navigate(`/product/1`)}}>
               <img
                 src="https://covers.shakespeareandcompany.com/97805527/9780552779739.jpg"
                 className="b3"
                 alt="The Book Thief"
               />
-              <h3>The Book Thief</h3>
-              <p className="genre">Historical</p>
-              <p className="desc">
-                Follows Liesel, a young girl in Nazi Germany...
-              </p>
-              <p className="price">Rs. 449</p>
+              <div>
+                <h3>The Book Thief</h3>
+                <p className="genre">Historical</p>
+                <p className="desc">
+                  Follows Liesel, a young girl in Nazi Germany...
+                </p>
+                <p className="price">Rs. 449</p>
 
-              <button>Buy Now</button>
-              <button className="secondary">Add to Cart</button>
+                <button onClick={() => {navigate(`/checkout/1`)}}>Buy Now</button>
+                <button className="secondary">Add to Cart</button>
+              </div>
             </div>
 
             {/* BOOK 4 */}
-            <div className="book-card">
+            <div className="book-card" onClick={() => {navigate(`/product/1`)}}>
               <img
                 src="https://m.media-amazon.com/images/I/91OINeHnJGL._AC_UF1000,1000_QL80_.jpg"
                 className="b4"
                 alt="Harry Potter"
               />
-              <h3>Harry Potter</h3>
-              <p className="genre">Horror</p>
-              <p className="desc">Harry discovers the magical world...</p>
-              <p className="price">Rs. 399</p>
+              <div>
+                <h3>Harry Potter</h3>
+                <p className="genre">Horror</p>
+                <p className="desc">Harry discovers the magical world...</p>
+                <p className="price">Rs. 399</p>
 
-              <button>Buy Now</button>
-              <button className="secondary">Add to Cart</button>
+                <button onClick={() => {navigate(`/checkout/1`)}}>Buy Now</button>
+                <button className="secondary">Add to Cart</button>
+              </div>
             </div>
           </div>
         </section>
